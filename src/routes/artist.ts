01be73cbbc840116100a0b539e810c8899afde95
mrkot1359 <mrkot1359@proton.me>
2023-04-12 14:24:56 +0700

    refactor: artist

--- src/routes/artist.ts
+++ src/routes/artist.ts
@@ -1,20 +1,18 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import * as ethers from "ethers";
 import express from "express";
+import isAuthorized from "~/middlewares/isAuthorized";
+import { followsToArrays } from "~/utils";
 
 const prisma = new PrismaClient();
 
 const router = express.Router();
 
-router.post("/:toFollow/:followedBy", async (req, res) => {
-  console.log("/follow=>", req.params);
-  const { toFollow: toFollowAddress, followedBy: followedByAddress } =
-    req.params;
+router.get("/follow/:toFollow", isAuthorized, async (req, res) => {
+  const address = res.locals.address;
+  const { toFollow } = req.params;
 
-  if (
-    !ethers.utils.isAddress(toFollowAddress) ||
-    !ethers.utils.isAddress(followedByAddress)
-  ) {
+  if (!ethers.utils.isAddress(toFollow)) {
     return res.status(400).json({ error: "Invalid address" });
   }
 
@@ -23,36 +21,39 @@ router.post("/:toFollow/:followedBy", async (req, res) => {
       data: {
         follower: {
           connectOrCreate: {
-            where: { address: followedByAddress },
-            create: { address: followedByAddress },
+            where: { address },
+            create: { address },
           },
         },
         following: {
           connect: {
-            address: toFollowAddress,
+            address: toFollow,
           },
         },
       },
     })
     .then((newFollow) => {
       console.log(newFollow);
-      return res.status(200).json({ newFollow });
+      return res.status(200).json({ ok: true });
     })
     .catch((e) => {
-      console.log(e);
-      return res.status(400).json({ error: "Unknown Error" });
+      if (e.code === "P2025") {
+        return res.status(404).json({ error: "Artist not found" });
+      } else if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        console.log(`Prisma Error ${e.code}: ${e.message}`);
+        return res.status(400).json({ error: "Artist follow error" });
+      } else {
+        console.log(e);
+        return res.status(400).json({ error: "Unknown Error" });
+      }
     });
 });
 
-router.post("/unfollow/:toFollow/:followedBy", async (req, res) => {
-  // console.log("unfollow");
-  const { toFollow: toFollowAddress, followedBy: followedByAddress } =
-    req.params;
+router.get("/unfollow/:toUnfollow", isAuthorized, async (req, res) => {
+  const address = res.locals.address;
+  const { toUnfollow } = req.params;
 
-  if (
-    !ethers.utils.isAddress(toFollowAddress) ||
-    !ethers.utils.isAddress(followedByAddress)
-  ) {
+  if (!ethers.utils.isAddress(toUnfollow)) {
     return res.status(400).json({ error: "Invalid address" });
   }
 
@@ -60,18 +61,25 @@ router.post("/unfollow/:toFollow/:followedBy", async (req, res) => {
     .delete({
       where: {
         followerId_followingId: {
-          followerId: followedByAddress,
-          followingId: toFollowAddress,
+          followerId: address,
+          followingId: toUnfollow,
         },
       },
     })
     .then((deletedFollow) => {
       console.log(deletedFollow);
-      return res.status(200).json({ deletedFollow });
+      return res.status(200).json({ ok: true });
     })
     .catch((e) => {
-      console.log(e);
-      return res.status(400).json({ error: "Unknown Error" });
+      if (e.code === "P2025") {
+        return res.status(404).json({ error: "Artist not found" });
+      } else if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        console.log(`Prisma Error ${e.code}: ${e.message}`);
+        return res.status(400).json({ error: "Artist unfollow error" });
+      } else {
+        console.log(e);
+        return res.status(400).json({ error: "Unknown Error" });
+      }
     });
 });
 
@@ -91,6 +99,7 @@ router.get("/:address", async (req, res) => {
         following: true,
       },
     })
+    .then((artist) => followsToArrays(artist))
     .then((artist) => {
       return res.status(200).json({ artist });
     })
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,21 @@
 import { createHash } from "node:crypto";
-import type { TagsOnTracks } from "@prisma/client";
+import type { Follows, TagsOnTracks } from "@prisma/client";
 
 type HasTags = { tags: TagsOnTracks[] };
+type HasFollows = { following: Follows[]; followedBy: Follows[] };
 
 export function tagsToArray<T>(track: T & HasTags) {
   return {
     ...track,
     tags: track.tags.map((tag) => tag.tagName),
+  };
+}
+
+export function followsToArrays<T>(artist: T & HasFollows) {
+  return {
+    ...artist,
+    following: artist.following.map(({ followerId }) => followerId),
+    followedBy: artist.followedBy.map(({ followingId }) => followingId),
   };
 }
 
