a33ba8e104d05a362e79cf716ae920a4dc988ef5
mrkot1359 <mrkot1359@proton.me>
2023-11-15 17:07:12 +0700

    feat: remove track from playlist

--- src/routes/playlists.ts
+++ src/routes/playlists.ts
@@ -207,60 +207,131 @@ router.delete("/:address/:slug", isAuthorized, isAddress, async (req, res) => {
 });
 
 // Add track to playlist
-router.post("/:address/:slug", isAuthorized, isAddress, async (req, res) => {
-  const { address, slug } = req.params;
-  const { trackId } = req.body;
+router.post(
+  "/:address/:slug/tracks",
+  isAuthorized,
+  isAddress,
+  async (req, res) => {
+    const { address, slug } = req.params;
+    const { trackId } = req.body;
 
-  if (address !== res.locals.address) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+    if (address !== res.locals.address) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
 
-  // Validation
-  // ========================================
-  if (!trackId) {
-    return res.status(400).json({
-      error: "Invalid data",
-    });
-  }
-  // ========================================
+    // Validation
+    // ========================================
+    if (!trackId) {
+      return res.status(400).json({
+        error: "Invalid data",
+      });
+    }
+    // ========================================
 
-  await prisma.tracksInPlaylists
-    .create({
-      data: {
-        track: { connect: { id: trackId } },
-        playlist: {
-          connect: { userId_slug: { slug, userId: address } },
+    await prisma.tracksInPlaylists
+      .create({
+        data: {
+          track: { connect: { id: trackId } },
+          playlist: {
+            connect: { userId_slug: { slug, userId: address } },
+          },
         },
-      },
-      select: {
-        playlist: {
-          select: PLAYLIST_FIELDS,
+        select: {
+          playlist: {
+            select: PLAYLIST_FIELDS,
+          },
         },
-      },
-    })
-    .then((playlist) => playlist.playlist)
-    .then((playlist) => ({
-      ...playlist,
-      createdAt: +playlist.createdAt,
-      updatedAt: +playlist.updatedAt,
-      tracks: playlist.tracks.map(({ track }) =>
-        collectTags(countPlayEvents(timestampToNumber(track)))
-      ),
-    }))
-    .then((playlist) => {
-      return res.status(200).json({ playlist });
-    })
-    .catch((e) => {
-      if (e.code === "P2002") {
-        return res.status(400).json({ error: "Track already in the playlist" });
-      } else if (e instanceof Prisma.PrismaClientKnownRequestError) {
-        console.log(`Prisma Error ${e.code}: ${e.message}`);
-        return res.status(400).json({ error: "Playlist updating error" });
-      } else {
-        console.log(e);
-        return res.status(400).json({ error: "Unknown Error" });
-      }
-    });
-});
+      })
+      .then((playlist) => playlist.playlist)
+      .then((playlist) => ({
+        ...playlist,
+        createdAt: +playlist.createdAt,
+        updatedAt: +playlist.updatedAt,
+        tracks: playlist.tracks.map(({ track }) =>
+          collectTags(countPlayEvents(timestampToNumber(track)))
+        ),
+      }))
+      .then((playlist) => {
+        return res.status(200).json({ playlist });
+      })
+      .catch((e) => {
+        if (e.code === "P2002") {
+          return res
+            .status(400)
+            .json({ error: "Track already in the playlist" });
+        } else if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          console.log(`Prisma Error ${e.code}: ${e.message}`);
+          return res.status(400).json({ error: "Playlist updating error" });
+        } else {
+          console.log(e);
+          return res.status(400).json({ error: "Unknown Error" });
+        }
+      });
+  }
+);
+
+// Remove track from playlist
+router.delete(
+  "/:address/:slug/tracks",
+  isAuthorized,
+  isAddress,
+  async (req, res) => {
+    const { address, slug } = req.params;
+    const { trackId } = req.body;
+
+    if (address !== res.locals.address) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // Validation
+    // ========================================
+    if (!trackId) {
+      return res.status(404).json({
+        error: "trackId not specified",
+      });
+    }
+    // ========================================
+
+    const playlistId = await prisma.playlist
+      .findUnique({
+        where: { userId_slug: { userId: address, slug } },
+      })
+      .then((playlist) => playlist?.id)
+      .catch((e) => {
+        if (e.code === "P2025") {
+          res.status(404).json({ error: "Playlist not found" });
+        } else if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          console.log(`Prisma Error ${e.code}: ${e.message}`);
+          res.status(400).json({ error: "Playlist updating error" });
+        } else {
+          console.log(e);
+          res.status(400).json({ error: "Unknown Error" });
+        }
+      });
+
+    if (!playlistId) return;
+
+    await prisma.tracksInPlaylists
+      .delete({
+        where: { trackId_playlistId: { trackId, playlistId } },
+      })
+      .then(() => {
+        return res.status(200).json({ ok: true });
+      })
+      .catch((e) => {
+        if (e.code === "P2025") {
+          return res
+            .status(404)
+            .json({ error: "Track is not in the playlist" });
+        } else if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          console.log(`Prisma Error ${e.code}: ${e.message}`);
+          return res.status(400).json({ error: "Playlist updating error" });
+        } else {
+          console.log(e);
+          return res.status(400).json({ error: "Unknown Error" });
+        }
+      });
+  }
+);
 
 export default router;
